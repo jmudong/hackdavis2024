6bd67541e18a11ed179882b1e713af6ac5f28008
allison2368 <allisonpeng10@gmail.com>
2024-04-28 03:47:08 -0700

    database connected

--- server/index.js
+++ server/index.js
@@ -1,9 +1,21 @@
 const path = require("path");
+const mongoose = require('mongoose');
+const morgan = require('morgan');
 const express = require("express");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
+
+// Step 2
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://allisonpeng10:1VahcvwwcYyff9kN@cluster0.5epcco7.mongodb.net/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!!!!');
+});
 
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
