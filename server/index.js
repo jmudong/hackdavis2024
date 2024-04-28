608ee7a41ce85e07e9e3edb3155791256bdf6071
Jasper Dong <116551997+jmudong@users.noreply.github.com>
2024-04-28 06:44:04 -0700

    modifying server/index.js

--- server/index.js
+++ server/index.js
@@ -1,11 +1,17 @@
 const path = require("path");
 const mongoose = require('mongoose');
+const cors = require('cors');
 const morgan = require('morgan');
 const express = require("express");
+const bodyParser = require('body-parser');
+require('./database');
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(cors())
 
 // Step 2
 mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://allisonpeng10:1VahcvwwcYyff9kN@cluster0.5epcco7.mongodb.net/', {
@@ -17,13 +23,15 @@ mongoose.connection.on('connected', () => {
     console.log('Mongoose is connected!!!!');
 });
 
-app.use(express.static(path.resolve(__dirname, '../client/build')));
+// API
+const users = require('/api/users');
+app.use('/api/users', users)
 
-app.get("/api", (req, res) => {
-  res.json({ message: "hi guys iim tired" });
+app.use(express.static(path.join(__dirname, '../build')));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, '../build'));
 });
 
-
 app.listen(PORT, () => {
-  console.log(`Server listening on ${PORT}`);
+  console.log(`Server started on ${PORT}`);
 });
