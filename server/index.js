971b765a485cd34fa809d78d556396c7e64e9d13
Jasper Dong <116551997+jmudong@users.noreply.github.com>
2024-04-28 07:25:46 -0700

    go back

--- server/index.js
+++ server/index.js
@@ -1,27 +1,29 @@
 const path = require("path");
 const mongoose = require('mongoose');
-const cors = require('cors');
 const morgan = require('morgan');
 const express = require("express");
-const bodyParser = require('body-parser');
-require('./database');
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(cors())
-
-// API
-const users = require('/api/users');
-app.use('/api/users', users)
-
-app.use(express.static(path.join(__dirname, '../build')));
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, '../build'));
+// Step 2
+mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://allisonpeng10:1VahcvwwcYyff9kN@cluster0.5epcco7.mongodb.net/', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
 });
 
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose is connected!!!!');
+});
+
+app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/api", (req, res) => {
+  res.json({ message: "hi guys iim tired" });
+});
+
+
 app.listen(PORT, () => {
-  console.log(`Server started on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
