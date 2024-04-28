91927cf618a060e9c75d8c27e761446370821ba8
allison2368 <allisonpeng10@gmail.com>
2024-04-28 02:09:28 -0700

    connect mongodb

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 const path = require("path");
 const express = require("express");
-
+const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+// Connect to MongoDB Atlas cluster
+mongoose.connect("mongodb+srv://allisonpeng10:Pandajojo25*@cluster0.5epcco7.mongodb.net/?\
+retryWrites=true&w=majority&appName=Cluster0", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+.then(() => {
+  console.log("Connected to MongoDB Atlas");
+})
+.catch((error) => {
+  console.error("Error connecting to MongoDB Atlas:", error);
+});
+
 app.use(express.static(path.resolve(__dirname, '../client/build')));
 
 app.get("/api", (req, res) => {
-  res.json({ message: "hi guys iim tired" });
+  res.json({ message: "hi guys i'm tired" });
 });
+
+app.listen(PORT, () => {
+  console.log(`Server listening on ${PORT}`);
+});
+
 
 
 app.listen(PORT, () => {
